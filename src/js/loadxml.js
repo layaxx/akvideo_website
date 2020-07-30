50efbf6a5d0525a5fe73deefc9c44cca1d9f5338
Yannick Lang <layaxx@gmx.de>
2020-07-30 21:52:45 +0200

    improve names on sitemap

--- src/js/loadxml.js
+++ src/js/loadxml.js
@@ -13,7 +13,8 @@ function dir(url) {
 }
 
 function dirchange(url, list) {
-    list += '</ul><p>' + dir(url) + '</p><ul>';
+    var name = dir(url);
+    list += '</ul><p>' + name + '</p><ul id="' + name + '">';
     return list;
 }
 
@@ -44,20 +45,7 @@ function setList(xml) {
         if (sitename === '404' || sitename === 'sitemap') {
             continue;
         }
-        switch (sitename) {
-            case '':
-                sitename = 'Startseite';
-                break;
-            case 'all':
-                sitename = 'Alle Projekte';
-                break;
-            case 'johns_adventure':
-                sitename = "John's Adventure";
-                break;
-            default:
-                sitename = sitename.replace('_', ' ');
-        }
-        sitename = capitalizeFLetter(sitename);
+        sitename = switchName(sitename);
 
         if (dirOld !== dir(url)) {
             list = dirchange(url, list);
@@ -67,4 +55,76 @@ function setList(xml) {
     }
     list += '</ul>';
     document.getElementsByClassName('sitemapcontainer')[0].innerHTML = list;
+    sortProjectList();
+}
+
+function switchName(sitename) {
+    switch (sitename) {
+        case '':
+            sitename = 'Startseite';
+            break;
+        case 'all':
+            sitename = 'Alle Projekte';
+            break;
+        case 'johns_adventure':
+            sitename = "John's Adventure";
+            break;
+        case 'zddz':
+            sitename = 'Zusammen durch die Zeit';
+            break;
+        case 'waldstein':
+            sitename = 'Das Geheimnis des Waldsteins';
+            break;
+        case 'johns_adventure':
+            sitename = "John's Adventure";
+            break;
+        case 'projects':
+            sitename = 'Alle Projekte';
+            break;
+        case 'panzerknacker':
+            sitename = 'Die Panzerknacker schlagen zu';
+            break;
+        case 'probe':
+            sitename = 'Die Probe';
+            break;
+        case 'unterwelt':
+            sitename = 'M&uuml;nchbergs TRAUMhafte Unterwelt';
+            break;
+        case 'stadtkasse':
+            sitename = 'Die geklaute Stadtkasse';
+            break;
+        case 'grundschule':
+            sitename = 'Workshops mit der Grundschule M&uuml;nchberg';
+            break;
+        case 'clipped':
+            sitename = 'Clipped - &Uuml;bersteuert';
+            break;
+        case 'stuhlgeschichten':
+            sitename = 'Stuhlgeschichten - Menschen auf St&uuml;hlen';
+            break;
+        default:
+            sitename = sitename.replace('_', ' ');
+    }
+    return capitalizeFLetter(sitename);
+}
+
+function sortProjectList() {
+    var list, i, switching, b, shouldSwitch;
+    list = document.getElementById('Projekte');
+    switching = true;
+    while (switching) {
+        switching = false;
+        b = list.getElementsByTagName('li');
+        for (i = 0; i < b.length - 1; i++) {
+            shouldSwitch = false;
+            if (b[i].firstElementChild.innerHTML.toLowerCase() > b[i + 1].firstElementChild.innerHTML.toLowerCase()) {
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            b[i].parentNode.insertBefore(b[i + 1], b[i]);
+            switching = true;
+        }
+    }
 }
