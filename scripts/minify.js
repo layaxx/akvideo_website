56cdec3693c6e8581293a207e5510dd2050979cc
Yannick Lang <layaxx@gmx.de>
2020-07-31 23:53:38 +0200

    minor fixes

--- scripts/minify.js
+++ scripts/minify.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 const path = require('path');
 
 const options = {
-    html: {
-        removeAttributeQuotes: false,
-        removeOptionalTags: false
-    }
+	html: {
+		removeAttributeQuotes: false,
+		removeOptionalTags: false
+	}
 };
 
 var minify = require('minify');
@@ -15,21 +15,23 @@ var dirPath = path.resolve(path.dirname(__filename), '../dist/.');
 minifyJS(dirPath);
 
 function minifyJS(dirPath) {
-    const files = fs.readdirSync(dirPath);
-    for (const file of files) {
-        const filePath = path.join(dirPath, file);
-        const stat = fs.statSync(filePath);
-        if (stat.isFile() && (file.endsWith('.js') || file.endsWith('.html') || file.endsWith('.css'))) {
-            console.log('Minifying: ' + filePath);
-            minify(filePath, options)
-                .then((content) =>
-                    fs.writeFile(filePath, content, function(err) {
-                        if (err) throw err;
-                    })
-                )
-                .catch(console.error);
-        } else if (stat.isDirectory()) {
-            minifyJS(filePath);
-        }
-    }
+	flag = false;
+	const files = fs.readdirSync(dirPath);
+	for (const file of files) {
+		const filePath = path.join(dirPath, file);
+		const stat = fs.statSync(filePath);
+		if (stat.isFile() && (file.endsWith('.js') || file.endsWith('.html') || file.endsWith('.css'))) {
+			flag = true;
+			minify(filePath, options)
+				.then((content) =>
+					fs.writeFile(filePath, content, function(err) {
+						if (err) throw err;
+					})
+				)
+				.catch(console.error);
+		} else if (stat.isDirectory()) {
+			minifyJS(filePath);
+		}
+	}
+	if (flag) console.log('### INFO: minified files in directory: ' + dirPath);
 }
